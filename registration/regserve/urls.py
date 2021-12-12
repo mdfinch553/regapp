17a0356ba499b46a0e5822308705badfa96d20ab
Michael Finch <mikefinch330@gmail.com>
2021-12-11 16:18:45 -0800

    Delete and Edit Students

--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -7,6 +7,8 @@ app_name = 'regserve'
 urlpatterns = [
     path('/students/', views.StudentListForm.as_view(), name="students"),
     path('/createstudent/', views.StudentCreateForm.as_view(), name="create_students"),
+    path('/<pk>/editstudent/', views.StudentEditForm.as_view(), name="edit_students"),
+    path('/<pk>/deletestudent/', views.StudentDeleteForm.as_view(), name="delete_students"),
     path('/data/students/', views.StudentListCreate.as_view()),
     path('', views.index, name='index')
 ]
--- registration/regserve/views.py
+++ registration/regserve/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import DeleteView
 from rest_framework import generics
 from .serializers import *
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 def index(request):
     return HttpResponse("Hello from django backend")
@@ -17,3 +18,12 @@ class StudentListForm(ListView):
 class StudentCreateForm(CreateView, ListView):
     model = Student
     fields = ['firstname', 'lastname', 'idnumber', 'schoolyear', 'major', 'gpa']
+
+class StudentEditForm(UpdateView):
+    model = Student
+    template_name = 'regserve/student_edit_form.html'
+    fields = ['firstname', 'lastname', 'idnumber', 'schoolyear', 'major', 'gpa']
+class StudentDeleteForm(DeleteView):
+    model = Student
+    template_name = 'regserve/student_delete_form.html'
+    success_url="/regserve/students/"
