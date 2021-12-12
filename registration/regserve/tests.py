ba2b1eb6e0447c243c1f5e5f0266f72c56771210
Michael Finch <mikefinch330@gmail.com>
2021-12-11 18:10:41 -0800

    Made logs for tests

--- registration/regserve/tests.py
+++ registration/regserve/tests.py
@@ -4,8 +4,22 @@ from .models import *
 import io
 from rest_framework.parsers import JSONParser
 from .serializers import *
+import logging
 
 class DataTest(TestCase):
+    def log_setup(self, logger_name, log_file, mode='w'):
+        new_log = logging.getLogger(logger_name)
+        formatter = logging.Formatter('%(asctime)s : %(message)s')
+        file_handler = logging.FileHandler(log_file, mode=mode)
+        file_handler.setFormatter(formatter)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        new_log.setLevel(logging.DEBUG)
+        new_log.addHandler(file_handler)
+        new_log.addHandler(stream_handler)
+        return new_log
+
     def setUp(self):
         Student.objects.create(
             firstname = "First",
@@ -27,29 +41,31 @@ class DataTest(TestCase):
         )
         self.test_client = Client()
     def test_student_api(self):
+        test_stuent_api_log = self.log_setup('student_api', 'test_logs/student_api.log', 'w')
         students_response = self.test_client.get('/regserve/data/students/')
-        print(f'STUDENT API TEST: insde test, response is {students_response}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, response is {students_response}\n')
         self.assertEqual(students_response.status_code, 200)
-        print(f'STUDENT API TEST: insde test, response content is {students_response.content}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, response content is {students_response.content}\n')
         student_stream = io.BytesIO(students_response.content)
-        print(f'STUDENT API TEST: insde test, student stream is {student_stream}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, student stream is {student_stream}\n')
         student_api_data = JSONParser().parse(stream=student_stream)
-        print(f'STUDENT API TEST: insde test, student api data is {student_api_data}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, student api data is {student_api_data}\n')
         first_student_data = student_api_data[0]
-        print(f'STUDENT API TEST: insde test, first student data is {first_student_data} id is {first_student_data["id"]}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, first student data is {first_student_data} id is {first_student_data["id"]}\n')
         first_student_db = Student.objects.get(id=first_student_data['id'])
-        print(f'STUDENT API TEST: insde test, first student db is {first_student_db}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, first student db is {first_student_db}\n')
         first_student_serializer = StudentSerializer(first_student_db, data=first_student_data)
-        print(f'STUDENT API TEST: insde test, first student serializer is {first_student_serializer}\n')
-        print(f'STUDENT API TEST: insde test, first student serializer is valid? {first_student_serializer.is_valid()}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, first student serializer is {first_student_serializer}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, first student serializer is valid? {first_student_serializer.is_valid()}\n')
         first_student_api = first_student_serializer.save()
-        print(f'STUDENT API TEST: insde test, first student api is {first_student_api}\n')
+        test_stuent_api_log.info(f'STUDENT API TEST: insde test, first student api is {first_student_api}\n')
         self.assertEqual(first_student_api, first_student_db)
 
     def test_student(self):
+        test_stuent_log = self.log_setup('student', 'test_logs/student.log', 'w')
         student_list = Student.objects.all()
         for student in student_list: 
-            print(f'Inside test student current student is {student}')
+            test_stuent_log.info(f'Inside test student current student is {student}')
         student = student_list[0]
         self.assertEqual(student.id, 1)
         self.assertEqual(student.full_name, 'First Student')
@@ -58,10 +74,24 @@ class DataTest(TestCase):
         self.assertEqual(student.gpa, 4.0)
 
 class SimpleTest(TestCase):
+    def log_setup(self, logger_name, log_file, mode='w'):
+        new_log = logging.getLogger(logger_name)
+        formatter = logging.Formatter('%(asctime)s : %(message)s')
+        file_handler = logging.FileHandler(log_file, mode=mode)
+        file_handler.setFormatter(formatter)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        new_log.setLevel(logging.DEBUG)
+        new_log.addHandler(file_handler)
+        new_log.addHandler(stream_handler)
+        return new_log
+
     def setUp(self):
         self.test_client = Client()
     def test_response(self):
+        test_response_log = self.log_setup('response', 'test_logs/response.log', 'w')
         response = self.test_client.get('/regserve')
-        print(f'Inside HW test, response is {response}')
+        test_response_log.info(f'Inside HW test, response is {response}')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b"Hello from django backend")
