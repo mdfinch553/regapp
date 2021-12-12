1f929721cf1176d0ff40fb3c56dcda5851fa2b43
Michael Finch <mikefinch330@gmail.com>
2021-12-11 17:54:00 -0800

    Modified Editing Forms

--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -7,7 +7,9 @@ app_name = 'regserve'
 urlpatterns = [
     path('/students/', views.StudentListForm.as_view(), name="students"),
     path('/createstudent/', views.StudentCreateForm.as_view(), name="create_students"),
+    path('/editstudent/', views.SelectStudentToEditForm.as_view(), name="select_students_to_edit"),
     path('/<pk>/editstudent/', views.StudentEditForm.as_view(), name="edit_students"),
+    path('/deletestudent/', views.SelectStudentToDeleteForm.as_view(), name="select_students_to_delete"),
     path('/<pk>/deletestudent/', views.StudentDeleteForm.as_view(), name="delete_students"),
     path('/data/students/', views.StudentListCreate.as_view()),
     path('', views.index, name='index')
--- registration/regserve/views.py
+++ registration/regserve/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.views.generic.edit import DeleteView
 from rest_framework import generics
 from .serializers import *
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django import forms
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
 
 def index(request):
     return HttpResponse("Hello from django backend")
@@ -23,7 +24,47 @@ class StudentEditForm(UpdateView):
     model = Student
     template_name = 'regserve/student_edit_form.html'
     fields = ['firstname', 'lastname', 'idnumber', 'schoolyear', 'major', 'gpa']
+
 class StudentDeleteForm(DeleteView):
     model = Student
     template_name = 'regserve/student_delete_form.html'
     success_url="/regserve/students/"
+
+class SelectStudentForm(forms.Form):
+    student_id = forms.IntegerField()
+
+class SelectStudentToDeleteForm(FormView, ListView):
+    model = Student
+    form_class = SelectStudentForm
+    template_name = 'regserve/select_students_to_delete_form.html'
+    student_id = 0
+
+    def form_valid(self, form):
+        self.get_student_id(form.cleaned_data)
+        self.get_success_url()
+        return super(SelectStudentToDeleteForm, self).form_valid(form)
+
+    def get_student_id(self, valid_data):
+        self.student_id = valid_data["student_id"]
+        print("student_id_is", self.student_id)
+    
+    def get_success_url(self):
+        return f'/regserve/{self.student_id}/deletestudent'
+
+class SelectStudentToEditForm(FormView, ListView):
+    model = Student
+    form_class = SelectStudentForm
+    template_name = 'regserve/select_students_to_edit_form.html'
+    student_id = 0
+
+    def form_valid(self, form):
+        self.get_student_id(form.cleaned_data)
+        self.get_success_url()
+        return super(SelectStudentToEditForm, self).form_valid(form)
+
+    def get_student_id(self, valid_data):
+        self.student_id = valid_data["student_id"]
+        print("student_id_is", self.student_id)
+    
+    def get_success_url(self):
+        return f'/regserve/{self.student_id}/editstudent'
